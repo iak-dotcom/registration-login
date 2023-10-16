7cdeb6d76ce3ebaf5773c2177f481ad47c8ab28c
imranahmedkhan@hotmail.co.uk <imranahmedkhan@hotmail.co.uk>
2023-10-16 12:35:06 +0100

    ManyToMany Relationship defined

--- src/main/java/com/khan/lr/model/User.java
+++ src/main/java/com/khan/lr/model/User.java
@@ -1,13 +1,28 @@
 package com.khan.lr.model;
 
+import java.util.Collection;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name =  "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class User {
@@ -24,4 +39,12 @@ public class User {
 	private String email;
 
 	private String password;
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(
+			name = "users_roles",
+			joinColumns = @JoinColumn(
+		            name = "user_id", referencedColumnName = "id"),
+			inverseJoinColumns = @JoinColumn(
+				            name = "role_id", referencedColumnName = "id"))
+	private Collection<Role> roles;
 }
